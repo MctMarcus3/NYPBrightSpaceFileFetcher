4e073cfe5fa9ea446965a53e1e20d78f7cac4c68
Mctmarcus <mctmarcus3@gmail.com>
2022-06-09 02:13:30 +0800

    Added Option to get cookie from browser

--- uff/setup_config.py
+++ uff/setup_config.py
@@ -1,29 +1,44 @@
 import json
+import os
 
-import inquirer
-from inquirer import Path
-
+from InquirerPy import inquirer
+from InquirerPy.validator import PathValidator
 from uff.brightspace import BrightspaceAPI
 from uff.courses import get_courses_list
 from uff.ufora_login import get_session
 
 
 def setup():
-    email = inquirer.text(message="What's your email address")
-    password = inquirer.password(message="What's your password")
-    otc_secret = inquirer.password(message="What's your 2FA secret?")
+    email = inquirer.text(message="What's your email address").execute()
+    password = inquirer.secret(message="What's your password").execute()
+    otc_secret = inquirer.secret(message="What's your 2FA secret?", default="").execute()
+    browser = inquirer.select(message="What's your browser (Used to get cookie from brightspace)?", choices=[
+                                                                          "Chrome",
+                                                                          "Firefox",
+                                                                          "Edge",
+                                                                          "Opera",
+                                                                          "Chromium"]).execute()
     session = get_session(email, password, otc_secret)
     if session is None:
         print("Invalid login credentials")
         return
-    config_file = inquirer.shortcuts.path(message="Specify config file", path_type=Path.FILE)
-    output_directory = inquirer.shortcuts.path(message="Specify output directory", path_type=Path.DIRECTORY)
-    brightspace_api = BrightspaceAPI(email, password, otc_secret)
-    courses = get_courses_list(brightspace_api)
-    selected_courses = inquirer.checkbox(message="Select courses to sync (press enter when ready)",
+    defaultConfigPath = "./config.json" if os.name == "posix" else ".\\config.json"
+    defaultDirPath = "../" if os.name == "posix" else "..\\"
+    config_file = inquirer.filepath(
+        message="Specify config file location",
+        default=defaultConfigPath).execute()
+    output_directory=inquirer.filepath(
+        message="Specify output directory", 
+        only_directories=True,  
+        default=defaultDirPath).execute()
+    brightspace_api=BrightspaceAPI(email, password, otc_secret)
+    courses=get_courses_list(brightspace_api)
+    selected_courses=inquirer.checkbox(message="Select courses to sync (press enter when ready)",
                                          choices=courses
-                                         )
-    course_ids = [courses[course] for course in selected_courses]
+                                         ).execute()
+
+    course_ids=[courses[course] for course in selected_courses]
+
     with open(config_file, "w+") as f:
         f.write(json.dumps({
             "output_directory": output_directory,
@@ -31,7 +46,8 @@ def setup():
             "credentials": {
                 "email": email,
                 "password": password,
-                "otc_secret": otc_secret
+                "otc_secret": otc_secret,
+                "browser": browser
             }
         }, indent=4))
     print("Setup complete!")
