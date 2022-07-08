994977fe32becdacabe7a4be88edd974aed30ffe
Mctmarcus <mctmarcus3@gmail.com>
2022-07-08 12:15:16 +0800

    increment version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="NYPLMSFileFetcher",
-    version="0.1.1",
+    version="0.1.2",
     packages=["uff"],
     entry_points={
         "console_scripts": ['NYPLMSFileFetcher=uff.uforafilefetcher:run'],
